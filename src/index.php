b46602f20de7d274a5f1d00bd8782e60904609e7
Samuel Beato <samuelbeato7@gmail.com>
2024-04-26 11:15:23 -0400

    COMENZADO LA VAINA DE LA VAINA DE PHP. PD: ODIO PHP

---
+++ src/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>CRUD - ODIOPHP</title>
+</head>
+<body>
+    <form action="create.php" method="post">
+        <input type="text" name="name" placeholder="Name">
+        <input type="password" name="password" placeholder="Password">
+        <input type="text" name="role" placeholder="Role">
+        <input type="submit" value="Guardar">
+    </form>
+</body>
+</html>
---
+++ src/services/create.php
@@ -0,0 +1,13 @@
+
+<?php
+
+// Conexión a la base de datos
+$servername = "localhost";
+$username = "root";
+$port = 3306;
+$password = "123456789";
+$dbname = "Test1";
+
+
+
+?>
