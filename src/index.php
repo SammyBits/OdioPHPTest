6eaa48f9a6614c232c67d65f2cb42bc3f586c9b3
Samuel Beato <samuelbeato7@gmail.com>
2024-04-26 11:21:32 -0400

    Actualizar estilos y agregar funcionalidad de creación de usuarios

--- src/index.php
+++ src/index.php
@@ -1,11 +1,47 @@
 <!DOCTYPE html>
 <html lang="es">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>CRUD - ODIOPHP</title>
 </head>
+
 <body>
+    <style>
+        body,
+        html {
+            background-color: #272727;
+            color: #fff;
+        }
+
+        form {
+            margin-bottom: 20px;
+        }
+
+        h2 {
+            margin-top: 20px;
+        }
+
+        p {
+            color: #fff;
+        }
+
+        input[type="text"] {
+            margin-right: 10px;
+        }
+
+        input[type="submit"] {
+            margin-top: 10px;
+        }
+    </style>
+
+    <h2>Crear nuevo usuario</h2>
+    <?php
+    require ("./services/create.php");
+
+    ?>
+
     <form action="create.php" method="post">
         <input type="text" name="name" placeholder="Name">
         <input type="password" name="password" placeholder="Password">
@@ -13,4 +49,5 @@
         <input type="submit" value="Guardar">
     </form>
 </body>
+
 </html>
--- src/services/create.php
+++ src/services/create.php
@@ -1,4 +1,3 @@
-
 <?php
 
 // Conexión a la base de datos
@@ -8,6 +7,24 @@ $port = 3306;
 $password = "123456789";
 $dbname = "Test1";
 
+$conn = mysqli_connect($servername, $username, $password, null, $port);
+$conn->select_db($dbname);
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
 
+ // Proceso de creación de un nuevo usuario si se ha enviado el formulario
+ if (isset($_POST['submit'])) {
+    $name = $_POST['name'];
+    $password = $_POST['password'];
+    $role = $_POST['role'];
 
-?>
+    $sql = "INSERT INTO Users (Username, Password, Role) VALUES ('$name', '$password', '$role)";
+    if ($conn->query($sql) === TRUE) {
+        // Redirigir al usuario a una nueva página después de realizar la inserción
+        header("Location: " . $_SERVER['REQUEST_URI']);
+        exit();
+    } else {
+        echo "Error: " . $sql . "<br>" . $conn->error;
+    }
+}
